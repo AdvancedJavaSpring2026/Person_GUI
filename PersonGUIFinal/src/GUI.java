d845f910036de2e8f5bb23b8a14118f92ab9181a
A103Productions <a103productions@gmail.com>
2026-04-21 15:39:30 -0500

    Main creates GUI

--- PersonGUIFinal/src/GUI.java
+++ PersonGUIFinal/src/GUI.java
@@ -1,2 +1,25 @@
-public class GUI {
+import javax.swing.*;
+
+public class GUI extends JFrame{
+
+    private static final int WIDTH = 1024;
+    private static final int HEIGHT = 1024;
+
+    JPanel jPanFullApp, jPanPersonArea;
+
+    public GUI(){
+
+
+
+
+        setSize(WIDTH, HEIGHT);
+        setResizable(false);
+        setVisible(true);
+
+    }
+
+
+
+
+
 }
