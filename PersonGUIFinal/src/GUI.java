845f512213510b075371880f21c57b00e67edd06
A103Productions <a103productions@gmail.com>
2026-04-21 15:42:27 -0500

    small adjustments
    slight update to not-UML doc

--- PersonGUIFinal/src/GUI.java
+++ PersonGUIFinal/src/GUI.java
@@ -2,13 +2,15 @@ import javax.swing.*;
 
 public class GUI extends JFrame{
 
+
     private static final int WIDTH = 1024;
     private static final int HEIGHT = 1024;
 
     JPanel jPanFullApp, jPanPersonArea;
 
     public GUI(){
-
+        super("Temporary Title");
+        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
 
 
 
